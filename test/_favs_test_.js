13b7f7c7b16de4124f32072025789f742d8c8a43
Coteh <james_c_34@live.com>
2016-08-19 17:43:33 -0400

    Add some tests.

--- test/_favs_test_.js
+++ test/_favs_test_.js
@@ -1,20 +1,73 @@
 var should = require('should');
+var mockery = require('mockery');
+var hackaFavs = null;
+
+var fsMock = {
+    readFileSync: function(path, options) {
+        return "{\"ids\":[1, 2, 3, 4, 5, 6]}";
+    }
+}
+
+var osMock = {
+    homedir: function() {
+        return "/home/james";
+    }
+}
+
+var pathMock = {
+    normalize: function(path) {
+        return path;
+    }
+}
 
 describe("hacka-favs", function(){
-    describe("opening favourites file", function(){
-        it("should open a file that contains favourite post IDs and add them to the program's current state", function(){
-            should.fail();
-        });
-    });
     describe("properties", function(){
+        var testArr = [1, 2, 3, 4, 5, 6];
+
+        before(function(){
+            mockery.enable({
+                useCleanCache: true
+            });
+            mockery.registerMock("os", osMock);
+            mockery.registerMock("path", pathMock);
+            mockery.registerAllowable("fs");
+            mockery.registerAllowable("../src/hacka-favs");
+            hackaFavs = require("../src/hacka-favs");
+            hackaFavs.setSavedPostIDS(testArr);
+        });
         it("should get the URL of the current designated filepath for the favourites file", function(){
-            should.fail();
+            hackaFavs.getSavedLocation().should.equal("/home/james/.hnsaved");
         });
         it("should get a list of saved post IDs", function(){
-            should.fail();
+            hackaFavs.getSavedIDs().should.deepEqual(testArr);
         });
         it("should get a saved post ID by positional index from the array, given an index", function(){
-            should.fail();
+            hackaFavs.getSavedPostID(4).should.equal(5);
+        });
+        after(function(){
+            mockery.deregisterMock("os");
+            mockery.deregisterMock("path");
+            mockery.disable();
+        });
+    });
+    describe("opening favourites file", function(){
+        before(function(){
+            mockery.enable({
+                useCleanCache: true
+            });
+            mockery.registerMock("fs", fsMock);
+            mockery.registerAllowable("os");
+            mockery.registerAllowable("path");
+            mockery.registerAllowable("../src/hacka-favs");
+            hackaFavs = require("../src/hacka-favs");
+        });
+        it("should add loaded post IDs to the program's current state", function(){
+            hackaFavs.openSavedPostsFile();
+            hackaFavs.getSavedIDs().should.deepEqual([1, 2, 3, 4, 5, 6]);
+        });
+        after(function(){
+            mockery.deregisterMock("fs");
+            mockery.disable();
         });
     });
     describe("saving posts", function(){
