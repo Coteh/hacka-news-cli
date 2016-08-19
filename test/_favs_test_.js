737fdabe36ff94c1f865a18d1a8fdaa697052bd1
Coteh <james_c_34@live.com>
2016-08-18 20:50:11 -0400

    Add blank failing tests. Add dev deps for mocha and should.

---
+++ test/_favs_test_.js
@@ -0,0 +1,35 @@
+var should = require('should');
+
+describe("hacka-favs", function(){
+    describe("opening favourites file", function(){
+        it("should open a file that contains favourite post IDs and add them to the program's current state", function(){
+            should.fail();
+        });
+    });
+    describe("properties", function(){
+        it("should get the URL of the current designated filepath for the favourites file", function(){
+            should.fail();
+        });
+        it("should get a list of saved post IDs", function(){
+            should.fail();
+        });
+        it("should get a saved post ID by positional index from the array, given an index", function(){
+            should.fail();
+        });
+    });
+    describe("saving posts", function(){
+        it("should be able to save a post ID to favourites file.", function(){
+            should.fail();
+        });
+    });
+    describe("unsaving posts", function(){
+        it("should be able to unsave a post ID from favourites file.", function(){
+            should.fail();
+        });
+    });
+    describe("saving posts by url", function(){
+        it("should take a Hacker News URL, extract the post ID from the URL, and save that to favourites", function(){
+            should.fail();
+        });
+    });
+});
